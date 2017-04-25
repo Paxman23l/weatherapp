1d2a81f3a86a01603ce3443d4faf033a3c60c853
Paxman23l <pgsagilbert@gmail.com>
2017-04-25 06:22:20 -0500

    database connection build

--- weatherstation.py
+++ weatherstation.py
@@ -1,15 +1,22 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 from time import sleep
+from datetime import datetime 
+#import pypyodbc
+#connection = pypyodbc.connect('DataSource=mssql5.gear.host; InitialCatalog=weatherapp; IntegratedSecurity=False; UserId= weatherapp; Password=Password1!; ConnectTimeout=15; Encrypt=false;TrustServerCertificate=True;ApplicationIntent=Write;MultiSubnetFailover=False')
+
+
+
 while True:
      t = sense.get_temperature()*1.8+32
      p = sense.get_pressure()
      h = sense.get_humidity()
+     time = datetime.now()
 
      t = round(t,1)
      p = round(p,1)
      h = round(h,1)
-
+     
      if t > 85 and t <200:
           bg = [100,0,0] #red
      if t >40 and t <85:
@@ -21,7 +28,11 @@ while True:
      temp = "T: {0}F".format(t)
      pres = "P: {0}in".format(p)
      hum = "H: {0}%".format(h)
+     time = "Time: {0}".format(time)
+     
      #sense.show_message(msg, scroll_speed=.065, back_colour=bg)
+     sense.show_message(time, scroll_speed=.07, back_colour=bg)
+     sleep(.01)
      sense.show_message(temp, scroll_speed=.07, back_colour=bg)
      sleep(.01)
      sense.show_message(pres, scroll_speed=.07, back_colour=bg)
