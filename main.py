7fdb51604435c01184b64ce26688d5c6babe0df6
Joel <gilbert.279@gmail.com>
2019-10-06 19:31:38 +0100

    adding coroutine

--- main.py
+++ main.py
@@ -50,8 +50,10 @@ def main():
     #     showMessage(round(temp_c, 1), .05)
     #     temp_f = convertToF(temp_c)
     #     showMessage(round(temp_f, 1), .05)
-    runWeatherTask = runWeather()
-    input("Press Enter to continue...")
+    runWeatherTask = asyncio.run(runWeather())
+    #input("Press Enter to continue...")
+    #asyncio.run(runWeatherTask)
+    input("Press Enter to quit...")
     runWeatherTask.close()
 
     
